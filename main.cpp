de2a8bfcec8804a6604c70255b92167b77172194
ali abbasi <aliabbasi806@gmail.com>
2021-11-18 03:55:23 -0800

    add map thread

--- main.cpp
+++ main.cpp
@@ -9,24 +9,14 @@ int main(int argc, char **argv){
     for (int i = 1; i < argc; i++) {
         files.push_back(argv[i]);
     }
-    // string dir,commands_file;
-    // if (argc == 3){
-    //     commands_file = argv[1];
-    //     dir = argv[2];
-    // }
-
-    // stringvec commands = read_csv(commands_file);
-
-    // for (int i = 0; i < commands.size(); ++i){
-    //     string pipe_address = "./pipes/f-" + to_string(i) + "-";
-    //     create_process(dir,pipe_address,commands[i],"./assets.out");
-    //     cout << to_string(read_from_fifo(pipe_address)) << endl;
-    // }
-
+    
     for (int i = 0; i < files.size(); i++) {
         string pipe_address = "./pipes/f-" + to_string(i) + "-";
-        create_process(files[i], pipe_address);
+        create_process(files[i], pipe_address, "./map.out");
+        cout << read_from_fifo(pipe_address) << endl;
     }
+
+    cout << "Bye" << endl;
 
 
     return 0;
--- utils.hpp
+++ utils.hpp
@@ -7,6 +7,7 @@
 #include<sys/types.h>
 #include<dirent.h>
 #include<vector>
+#include<map>
 #include <unistd.h>
 #include <fcntl.h> 
 #include <sys/stat.h> 
@@ -27,19 +28,13 @@ using namespace std;
 
 typedef std::vector<std::string> stringvec;
 stringvec read_csv(std::string filename);
-int return_date(char* date_str);
-std::vector<int> parse_command(std::string command);
 stringvec read_directory(const std::string& name);
 int read_from_fifo(string fifo_path);
 void write_to_fifo(int price, string fifo_path);
 int return_max(vector<int> vec);
 int return_min(vector<int> vec);
-int get_maxOrmin_between_childs(string command,stringvec dir_vec,string pipe_name);
+std::map<string, int> map_words(stringvec words);
 stringvec parse_input(char buf[SIZE]);
-void create_process(string province_name,string pipe_name);
-int get_max(stringvec csv_file, int product_id, int start_date, int end_date);
-int get_min(stringvec csv_file, int product_id, int start_date, int end_date);
-int calculate_result(string shop_name,string command);
-vector<int> parse_line(string line);
+void create_process(string province_name,string pipe_name, const char* argv);
 
 #endif
